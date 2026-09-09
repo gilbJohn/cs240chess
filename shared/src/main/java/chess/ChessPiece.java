b1a0c9f51c0160d318d5fbd1478100480b2b9dee
John G <gibby4457@gmail.com>
2026-09-08 20:08:38 -0600

    Following along with the videos. I have 7 tests passing and many more to go. I addedto string methods and definitions for several of the functions. Mainly just definitions and making sure that the positions return the right data. Have not implemented any movement logic

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Represents a single chess piece
@@ -48,6 +49,6 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        return List.of();
     }
 }
