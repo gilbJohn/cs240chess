7f9dbb797c50167c6c6fa1367d8f92db8e5c5f86
John G <gibby4457@gmail.com>
2026-09-11 14:56:38 -0600

    Finishing the video

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -11,6 +12,19 @@ import java.util.List;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
 
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
@@ -56,7 +70,81 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
 
+        // -- is going left up diagonal 1:1
+        // -+ is going right up diagonal 1:8
+        // +- going left down 8:1
+        // ++ going right down 8:8
         if (piece.getPieceType() == PieceType.BISHOP) {
+            List<ChessMove> moves = new ArrayList<>();
+
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+
+            while (row > 1 && col > 1) {
+                row--;
+                col--;
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+
+                moves.add(new ChessMove(myPosition, newPosition, null));
+
+            }
+
+            row = myPosition.getRow();
+            col = myPosition.getColumn();
+
+            while (row > 1 && col > 8) {
+                row--;
+                col++;
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+
+                moves.add(new ChessMove(myPosition, newPosition, null));
+
+            }
+
+            row = myPosition.getRow();
+            col = myPosition.getColumn();
+
+            while (row > 8 && col > 1) {
+                row++;
+                col--;
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+
+                moves.add(new ChessMove(myPosition, newPosition, null));
+
+            }
+
+            row = myPosition.getRow();
+            col = myPosition.getColumn();
+
+            while (row > 8 && col > 8) {
+                row++;
+                col++;
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+
+                moves.add(new ChessMove(myPosition, newPosition, null));
+
+            }
+
+
+            return moves;
+        }
+        if (piece.getPieceType() == PieceType.PAWN) {
+            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+        }
+        if (piece.getPieceType() == PieceType.KING) {
+            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+        }
+        if (piece.getPieceType() == PieceType.ROOK) {
+            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+        }
+        if (piece.getPieceType() == PieceType.QUEEN) {
+            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+        }
+        if (piece.getPieceType() == PieceType.KNIGHT) {
             return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
         }
         return List.of();
